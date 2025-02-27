8a42dc4efb944cb6c74bafd6ac62d47a419b9c12
JejmsW <50978385+JejmsW@users.noreply.github.com>
2025-02-27 18:03:05 +0800

    1.图片改为网络获取

--- lib/pages/time_off_detail_page.dart
+++ lib/pages/time_off_detail_page.dart
@@ -9,7 +9,6 @@ import 'package:statistics_app/utils/extensions/widget_extensions.dart';
 import '../model/show_record_model.dart';
 import '../utils/db/image_manager.dart'; // 导入 intl 来格式化时间
 
-
 class TimeOffDetailPage extends StatefulWidget {
   final ShowRecordModel record;
   const TimeOffDetailPage({super.key, required this.record});
@@ -30,7 +29,8 @@ class _TimeOffDetailPageState extends State<TimeOffDetailPage> {
   }
 
   void _loadImage() async {
-    ImageModel? image = await ImageManager.getImageById(record.recordModel.imageId);
+    ImageModel? image =
+        await ImageManager.getImageById(record.recordModel.imageId);
     if (image != null) {
       setState(() {
         _imageBytes = image.body;
@@ -40,14 +40,14 @@ class _TimeOffDetailPageState extends State<TimeOffDetailPage> {
 
   // 进入全屏图片浏览器页面
   void _showFullScreenImage(BuildContext context) {
-    if (_imageBytes != null) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => FullScreenImagePage(imageBytes: _imageBytes!),
-        ),
-      );
-    }
+    // if (_imageBytes != null) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenImagePage(),
+      ),
+    );
+    // }
   }
 
   @override
@@ -61,23 +61,61 @@ class _TimeOffDetailPageState extends State<TimeOffDetailPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('${record.recordModel.time.toStringAsFixed(1)}', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)).center(),
-            Text('员工名称: ${record.userModel.name}', style: TextStyle(fontSize: 18)),
-            Text('开始时间: ${record.recordModel.startTime}', style: TextStyle(fontSize: 18)),
-            Text('结束时间: ${record.recordModel.endTime}', style: TextStyle(fontSize: 18)),
-            Text('备注: ${record.recordModel.remarks}', style: TextStyle(fontSize: 18)),
-            if (_imageBytes != null) ...[
-              SizedBox(height: 20),
-              GestureDetector(
+            Text('${record.recordModel.time.toStringAsFixed(1)}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+                .center(),
+            Text('员工名称: ${record.userModel.name}',
+                style: TextStyle(fontSize: 18)),
+            Text('开始时间: ${record.recordModel.startTime}',
+                style: TextStyle(fontSize: 18)),
+            Text('结束时间: ${record.recordModel.endTime}',
+                style: TextStyle(fontSize: 18)),
+            Text('备注: ${record.recordModel.remarks}',
+                style: TextStyle(fontSize: 18)),
+            // if (_imageBytes != null) ...[
+            SizedBox(height: 20),
+            //https://img2.baidu.com/it/u=2849290704,1302860471&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=1085
+            GestureDetector(
                 onTap: () => _showFullScreenImage(context), // 点击图片查看大图
-                child: Image.memory(
-                  _imageBytes!, // 使用 Image.memory 显示二进制数据
+                // child: Image.memory(
+                //   _imageBytes!, // 使用 Image.memory 显示二进制数据
+                //   width: MediaQuery.of(context).size.width,
+                //   height: 300,
+                //   fit: BoxFit.cover,
+                // ),
+                // child: Image.network(
+                //   'https://www.w3schools.com/w3images/fjords.jpg', // 替换为你的图片 URL
+                //   // width: MediaQuery.of(context).size.width,
+                //   width: 300,
+                //   height: 300,
+                //   fit: BoxFit.cover,
+                // ),
+                child: Image.network(
+                  'https://www.w3schools.com/w3images/fjords.jpg',
                   width: MediaQuery.of(context).size.width,
                   height: 300,
                   fit: BoxFit.cover,
-                ),
-              ),
-            ]
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) {
+                      return child; // 图片加载完成
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  (loadingProgress.expectedTotalBytes ?? 1)
+                              : null,
+                        ),
+                      );
+                    }
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return Center(
+                      child: Text('加载图片失败'),
+                    );
+                  },
+                )),
+            // ]
           ],
         ),
       ),
@@ -87,9 +125,10 @@ class _TimeOffDetailPageState extends State<TimeOffDetailPage> {
 
 // 全屏图片页面
 class FullScreenImagePage extends StatelessWidget {
-  final Uint8List imageBytes;
+  // final Uint8List imageBytes;
 
-  const FullScreenImagePage({super.key, required this.imageBytes});
+  // const FullScreenImagePage({super.key, required this.imageBytes});
+  const FullScreenImagePage({super.key,});
 
   @override
   Widget build(BuildContext context) {
@@ -103,8 +142,12 @@ class FullScreenImagePage extends StatelessWidget {
             minScale: 0.1, // 最小缩放比例
             maxScale: 4.0, // 最大缩放比例
             child: Center(
-              child: Image.memory(
-                imageBytes,
+              // child: Image.memory(
+              //   imageBytes,
+              //   fit: BoxFit.contain, // 保持图片的宽高比
+              // ),
+              child: Image.network(
+                "https://www.w3schools.com/w3images/fjords.jpg", // 替换为图片的 URL
                 fit: BoxFit.contain, // 保持图片的宽高比
               ),
             ),
